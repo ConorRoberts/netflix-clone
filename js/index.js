fb45de3d94734d83759905ebb000dba24590cfd7
ConorRoberts <62777912+ConorRoberts@users.noreply.github.com>
2020-08-17 20:11:49 -0400

    Working on transitions for movie elements

--- js/index.js
+++ js/index.js
@@ -1,6 +1,14 @@
-async function getIpsum(){
-    let ipsum = await fetch("https://api.codetabs.com/v1/proxy?quest=http://loripsum.net/api/plaintext");
-    console.log(ipsum.json());
-}
+// async function getIpsum(){
+//     let ipsum = await fetch("https://api.codetabs.com/v1/proxy?quest=http://loripsum.net/api/plaintext");
+//     console.log(ipsum.json());
+// }
 
-getIpsum();
+// getIpsum();
+
+
+let dom = {
+    myList: document.querySelector(".my-list")
+};
+
+
+console.log(dom);
